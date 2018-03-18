3d700825e65bce28d55f1befb081cf08034d9103
victoriavo <vickivo11@yahoo.com>
2018-03-18 01:01:59 -0500

    Project 4: Mult progress

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,53 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//Multiplication in assembly is basically repeated addition
+//if we load R1's value, then we can set that to the "i" in a loop similar to a C/C++ loop
+//pseudocode:
+//int multiplier = R0
+//int multiplicand = R1
+//int product;
+//for(int i = multiplicand; i > 0; i--)
+//{
+//  product += multiplier;
+//}
+//R2 = product;
+
+
+//D = RAM[0]
+@R0
+D=M
+//multiplier = RAM[0]
+@multiplier
+M=D
+
+//D = RAM[1]
+@R1
+D=M
+//multiplicand = RAM[1]
+@multiplicand
+M=D
+
+//product = 0 (initialize product to 0)
+@product
+M=0
+
+//set D register to A
+@multiplicand
+D=A
+(Loop)
+    //product += multiplier
+    @multiplier
+    D=A
+    @product
+    MD=M+D
+    @Loop
+    D=D-1;JLE
+
+//RAM[2] = RAM[0] * RAM[1] (RAM[addr] = n)
+@R2
+D=A
+
+
+
