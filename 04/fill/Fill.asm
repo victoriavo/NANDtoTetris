8c56829584940c2ec2f40b3b228de695c9b1b4c2
victoriavo <vickivo11@yahoo.com>
2018-03-19 12:19:17 -0500

    Project 4: Fill progress

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,81 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@SCREEN
+D=A
+@screenpointer
+M=D
+
+(INFLOOP) //Infinite reading and checking loop
+//read from keyboard
+@KBD
+D=M
+@key
+M=D
+
+@DIRECTIVE
+0;JMP
+
+(CLEAR)
+//make the pixel equal to 0 (white)
+//decrement the screen pointer
+//RAM[memory_address] = 0
+@0
+D=A
+@screenpointer
+A=M
+@A
+M=D
+@16400
+D=A
+@screenpointer
+D=D-M
+@INFLOOP
+D;JEQ
+@16
+D=A
+@screenpointer
+M=M-D
+@INFLOOP //go back for updates
+0;JMP
+
+
+(FILL)
+//make the pixel equal to 1 (black)
+//increment the screen pointer
+//RAM[memory_address] = -1
+@-1
+D=A
+@screenpointer
+A=M
+@A
+M=D
+@131072
+D=A
+@screenpointer
+D=D-M
+@INFLOOP
+D;JEQ
+@16
+D=A
+@screenpointer
+M=M+D
+@INFLOOP //go back for updates
+0;JMP
+
+(DIRECTIVE)
+//if kbd ascii code is 0, then clear
+//else if ascii code is not 0, fill
+@key
+D=M
+@CLEAR
+D;JEQ
+@FILL
+0;JMP //go to location
+@INFLOOP //reiterate the infinite loop
+0;JMP
+
+
+
+(END)
